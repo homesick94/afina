0652976aff041661eba6e28d38874772e7abdf54
homesick94 <kt2608@gmail.com>
2017-11-01 19:42:30 +0300

    FIX: build

--- src/network/nonblocking/ServerImpl.cpp
+++ src/network/nonblocking/ServerImpl.cpp
@@ -33,50 +33,50 @@ ServerImpl::~ServerImpl() {}
 void ServerImpl::Start(uint32_t port, uint16_t n_workers) {
     std::cout << "network debug: " << __PRETTY_FUNCTION__ << std::endl;
 
-    // If a client closes a connection, this will generally produce a SIGPIPE
-    // signal that will kill the process. We want to ignore this signal, so send()
-    // just returns -1 when this happens.
-    sigset_t sig_mask;
-    sigemptyset(&sig_mask);
-    sigaddset(&sig_mask, SIGPIPE);
-    if (pthread_sigmask(SIG_BLOCK, &sig_mask, NULL) != 0) {
-        throw std::runtime_error("Unable to mask SIGPIPE");
-    }
+//    // If a client closes a connection, this will generally produce a SIGPIPE
+//    // signal that will kill the process. We want to ignore this signal, so send()
+//    // just returns -1 when this happens.
+//    sigset_t sig_mask;
+//    sigemptyset(&sig_mask);
+//    sigaddset(&sig_mask, SIGPIPE);
+//    if (pthread_sigmask(SIG_BLOCK, &sig_mask, NULL) != 0) {
+//        throw std::runtime_error("Unable to mask SIGPIPE");
+//    }
 
-    // Create server socket
-    struct sockaddr_in server_addr;
-    std::memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;         // IPv4
-    server_addr.sin_port = htons(port);       // TCP port number
-    server_addr.sin_addr.s_addr = INADDR_ANY; // Bind to any address
+//    // Create server socket
+//    struct sockaddr_in server_addr;
+//    std::memset(&server_addr, 0, sizeof(server_addr));
+//    server_addr.sin_family = AF_INET;         // IPv4
+//    server_addr.sin_port = htons(port);       // TCP port number
+//    server_addr.sin_addr.s_addr = INADDR_ANY; // Bind to any address
 
-    int server_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (server_socket == -1) {
-        throw std::runtime_error("Failed to open socket");
-    }
+//    int server_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+//    if (server_socket == -1) {
+//        throw std::runtime_error("Failed to open socket");
+//    }
 
-    int opts = 1;
-    if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &opts, sizeof(opts)) == -1) {
-        std::cout << errno << std::endl;
-        close(server_socket);
-        throw std::runtime_error("Socket setsockopt() failed");
-    }
+//    int opts = 1;
+//    if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &opts, sizeof(opts)) == -1) {
+//        std::cout << errno << std::endl;
+//        close(server_socket);
+//        throw std::runtime_error("Socket setsockopt() failed");
+//    }
 
-    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
-        close(server_socket);
-        throw std::runtime_error("Socket bind() failed");
-    }
+//    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+//        close(server_socket);
+//        throw std::runtime_error("Socket bind() failed");
+//    }
 
-    make_socket_non_blocking(server_socket);
-    if (listen(server_socket, 5) == -1) {
-        close(server_socket);
-        throw std::runtime_error("Socket listen() failed");
-    }
+//    make_socket_non_blocking(server_socket);
+//    if (listen(server_socket, 5) == -1) {
+//        close(server_socket);
+//        throw std::runtime_error("Socket listen() failed");
+//    }
 
-    for (int i = 0; i < n_workers; i++) {
-        workers.emplace_back(pStorage);
-        workers.back().Start(server_socket);
-    }
+//    for (int i = 0; i < n_workers; i++) {
+//        workers.emplace_back(pStorage);
+//        workers.back().Start(server_socket);
+//    }
 }
 
 // See Server.h
